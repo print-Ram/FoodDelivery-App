e28dcc87fc4c6f8cab67506c12325d45fe65fcda
print-ram <sreeramm653@gmail.com>
2025-05-06 16:50:00 +0530

    frontend updates

---
+++ frontend-angular/choco/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ModalComponent } from './modal/modal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ModalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend-angular/choco/src/app/models/food.ts
@@ -0,0 +1,7 @@
+export class Foods {
+  product_id!: string;
+  price!: number;
+  name!: string;
+  description!: string;
+  imageurl!: string;
+}
